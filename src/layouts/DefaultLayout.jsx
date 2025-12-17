43e0d80bda29abe7d0b5a204fe7da525527a1c33
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-12-17 16:54:30 +0100

    add header and footer

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
-export default function DefaultLayout() {
+export default function MainLayout() {
     return (
-
         <>
-            <header></header>
+            <Header />
 
-            <Outlet />
+            <main>
+                <Outlet />
+            </main>
 
-            <footer></footer>
+            <Footer />
         </>
-    )
+    );
 }
